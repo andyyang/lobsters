0535cbb87c74554c377409a0fe407a3da4f65ae6
Wayne <mingwei.y@gmail.com>
2014-05-16 07:16:34 +0800

    translate stories errors

--- app/models/story.rb
+++ app/models/story.rb
@@ -35,15 +35,14 @@ class Story < ActiveRecord::Base
         if self.new_record? && (s = Story.find_similar_by_url(self.url))
           self.already_posted_story = s
           if s.is_recent?
-            errors.add(:url, "has already been submitted within the past " <<
-              "#{RECENT_DAYS} days")
+            errors.add(:url, I18n.t("stories.validate.posted_story", recent_days: RECENT_DAYS))
           end
         end
       else
         errors.add(:url, "is not valid")
       end
     elsif self.description.to_s.strip == ""
-      errors.add(:description, t("stories.validate.description"))
+      errors.add(:description, I18n.t("stories.validate.description"))
     end
 
     check_tags
@@ -157,9 +156,7 @@ class Story < ActiveRecord::Base
 
     if !self.taggings.reject{|t| t.marked_for_destruction? || t.tag.is_media?
     }.any?
-      errors.add(:base, "Must have at least one non-media (PDF, video) " <<
-        "tag.  If no tags apply to your content, it probably doesn't " <<
-        "belong here.")
+      errors.add(:base, I18n.t("stories.validate.tags"))
     end
   end
 
