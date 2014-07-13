5008940b44ce80cac55d5c1f577ecc3fda6f9596
Wayne <mingwei.y@gmail.com>
2014-07-13 23:56:57 +0800

    transalte invitation request

--- app/mailers/invitation_request_mailer.rb
+++ app/mailers/invitation_request_mailer.rb
@@ -5,9 +5,16 @@ class InvitationRequestMailer < ActionMailer::Base
   def invitation_request(invitation_request)
     @invitation_request = invitation_request
 
+    if I18n.locale.to_s == "zh-CN"
+      template_name = 'invitation_request_zh-cn'
+    else
+      template_name = 'invitation_request'
+    end
+
     mail(
       :to => invitation_request.email,
-      subject: t("invitation_request_mailer.invitation_request.subject", application: Rails.application.name)
+      subject: t("invitation_request_mailer.invitation_request.subject", application: Rails.application.name),
+      template_name: template_name
     )
   end
 end
