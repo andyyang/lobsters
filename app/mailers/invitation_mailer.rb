7c4510d62d5a01bc917c9ed5498d842837890687
Wayne <mingwei.y@gmail.com>
2014-07-15 23:22:04 +0800

    translate invitation mail

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -5,10 +5,16 @@ class InvitationMailer < ActionMailer::Base
   def invitation(invitation)
     @invitation = invitation
 
+    if I18n.locale.to_s == "zh-CN"
+      template_name = 'invitation_zh-cn'
+    else
+      template_name = 'invitation'
+    end
+
     mail(
       :to => invitation.email,
-      subject: "[#{Rails.application.name}] You have been invited to " <<
-        Rails.application.name
+      subject: t("invitation_mailer.invitation.subject", application: Rails.application.name),
+      template_name: template_name
     )
   end
 end
